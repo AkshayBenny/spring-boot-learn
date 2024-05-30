1e676b6bb4576d87be8a3420644031f9f7084c36
akshay <akshaybennygh@gmail.com>
2024-05-30 03:46:46 +0100

    fetch and add data

--- Quiz-Application/src/main/java/com/akshay/Quiz/Application/dao/QuestionDao.java
+++ Quiz-Application/src/main/java/com/akshay/Quiz/Application/dao/QuestionDao.java
@@ -1,5 +1,15 @@
 package com.akshay.Quiz.Application.dao;
 
-public class QuestionDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.akshay.Quiz.Application.Question;
+import java.util.List;
+
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String category);
     
 }
